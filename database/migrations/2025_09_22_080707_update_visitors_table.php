706489c83f1c242931411d07bcfc1f80011b7f01
Laptop\Intel <hirakmehta007@gmail.com>
2025-09-22 13:42:43 +0530

    DB changes

---
+++ database/migrations/2025_09_22_080707_update_visitors_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('visitors', function (Blueprint $table) {
+            $table->string('email')->nullable()->change();
+            $table->string('mobile')->change();
+        });
+    }
+    
+    /**
+     * Reverse the migrations.
+    */
+    public function down(): void
+    {
+        Schema::table('visitors', function (Blueprint $table) {
+            $table->string('email')->change();
+            $table->string('mobile')->nullable(23)->change();
+
+        });
+    }
+};
